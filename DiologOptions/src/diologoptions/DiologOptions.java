3cac491274882effb1780e9d237d54ad5879666e
Victor Silva <victorhfsilva@protonmail.com>
2022-07-10 14:30:32 -0300

    Dialog with Options Example

---
+++ DiologOptions/src/diologoptions/DiologOptions.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package diologoptions;
+
+/**
+ *
+ * @author victor
+ */
+public class DiologOptions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new DialogOptionsJFrame().setVisible(true);
+    }
+    
+}
