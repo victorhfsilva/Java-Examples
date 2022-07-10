6576a4e62207303b5c394f0919058cdfebc2c560
Victor Silva <victorhfsilva@protonmail.com>
2022-07-10 20:00:20 -0300

    Console Input and Output Example

---
+++ ConsoleInputOutput/src/consoleinputoutput/ConsoleInputOutput.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package consoleinputoutput;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author victor
+ */
+public class ConsoleInputOutput {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean isInt = false;
+        while (isInt == false){
+            try {
+                System.out.println("Type a integer number.");
+                int intNum = input.nextInt();
+                isInt = true;
+                System.out.println("You typed the number: " + intNum);
+            }
+            catch (InputMismatchException exception) {
+                System.err.println("You did not type a integer.");
+                input.nextLine(); //Discard input for new attempt
+                System.out.println("Try again.");
+            }
+        }
+        
+    }
+    
+}
